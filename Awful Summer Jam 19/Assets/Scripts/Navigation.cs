655d19c5f61de80e698bbb71dfc4cbc2819d1bec
Millmoss <omelliff@yahoo.com>
2019-07-20 02:07:56 -0400

    Made a crappy navmesh script thing. Might just do something simpler though:

---
+++ Awful Summer Jam 19/Assets/Scripts/Navigation.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Navigation : MonoBehaviour
+{
+	public float unit = 1f;
+	public int size = 250;
+	public float castHeight = 3;
+	public GameObject goodMesh;
+	public GameObject badMesh;
+	public LayerMask castLayer;
+	private Vector3[,] positions;
+	private bool[,] walkable;
+
+	void OnEnable()
+	{
+		positions = new Vector3[size, size];
+		walkable = new bool[size, size];
+
+		for (int x = -size / 2; x < size / 2; x++)
+		{
+			for (int z = -size / 2; z < size / 2; z++)
+			{
+				Vector3 pos = transform.position + new Vector3(x, castHeight, z);
+				RaycastHit h;
+				if (Physics.Raycast(pos, Vector3.down, out h, castHeight + 0.01f, castLayer.value))
+				{
+					positions[x + size / 2, z + size / 2] = h.point;
+					walkable[x + size / 2, z + size / 2] = true;
+				}
+				else
+				{
+					positions[x + size / 2, z + size / 2] = Vector3.zero;
+					walkable[x + size / 2, z + size / 2] = false;
+				}
+			}
+		}
+	}
+
+	public List<Vector3> GetPath(Vector3 goal)		// TODO : consider a much simpler solution (travelling salesman
+	{
+		return null;
+	}
+}
