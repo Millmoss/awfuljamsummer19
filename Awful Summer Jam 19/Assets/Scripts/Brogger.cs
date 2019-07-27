8283fc6c152c3ebd0b2ba067d1beac172406d75c
Axel <stewaa5@rpi.edu>
2019-07-27 00:07:58 -0400

    Brogger behavior nearly complete - movement tuned. Wander behavior works again. Enters idle state when destination reached.

--- Awful Summer Jam 19/Assets/Scripts/Brogger.cs
+++ Awful Summer Jam 19/Assets/Scripts/Brogger.cs
@@ -6,15 +6,17 @@ public class Brogger : MonoBehaviour
 {
     public string state = "IDLE";
     private Vector3 home;
-    private Vector3 waypoint;
+    public Vector3 waypoint;
     public float movementSpeed = 5;
     private float currentSpeed;
     public GameObject p; // player
-    private float wanderTime = 0;
+    public float wanderTime = 0;
     public float maxWanderTime;
     public float wanderDistance;
     public float aggroDist;
     public Animator anim;
+    public float turnRate;
+    public float spinTime;
 
     // Start is called before the first frame update
     void Start() {
@@ -45,19 +47,26 @@ public class Brogger : MonoBehaviour
         }
 
         // Get ready for some shit
-        if(state == "ORIENTATION") {
+        if (state == "ORIENTATION")
+        {
+            waypoint = p.transform.position;
             wanderTime = wanderTime + Time.deltaTime;
-            if(wanderTime < 0.5f) {
-                // Face the player
-                transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y + wanderTime * wanderTime * 500, transform.eulerAngles.z);
-            } else {
-                transform.LookAt(p.transform);
+            if (wanderTime < spinTime)
+            {
+                transform.GetComponent<Rigidbody>().MoveRotation(Quaternion.Euler(new Vector3(0, transform.eulerAngles.y + wanderTime * wanderTime * 500, 0)));
+            }
+            else
+            {
+                Debug.Log(".");
+                // Okay, so get the look rotation toward the player, but then only use the Y component.
+                transform.GetComponent<Rigidbody>().MoveRotation(Quaternion.Euler(new Vector3(0, Quaternion.LookRotation(waypoint - transform.position, Vector3.up).eulerAngles.y, 0)));
                 state = "AGGRO";
+                wanderTime = 0;
             }
         }
 
         // If wandering, make sure there is one waypoint.
-        if(state == "WANDER") {
+        if (state == "WANDER") {
             // Debug.Log(wanderTime);
             // If a waypoint is inaccessible, allow it to time out.
             wanderTime = wanderTime + Time.deltaTime;
@@ -70,35 +79,79 @@ public class Brogger : MonoBehaviour
             // If too close, make a new one with random changes to x and z.
 
             // When moving, we don't want to be as urgent as if chasing the player.
-            if(Vector2.Distance(new Vector2(transform.position.x, transform.position.z), new Vector2(waypoint.x, waypoint.z)) > 0.1f) {
+            if(Vector2.Distance(new Vector2(transform.position.x, transform.position.z), new Vector2(waypoint.x, waypoint.z)) > 0.2f) {
                 currentSpeed = Mathf.Lerp(currentSpeed, movementSpeed / 3, movementSpeed / 3 * Time.deltaTime);
             } else {
-                currentSpeed = Mathf.Lerp(currentSpeed, 0, movementSpeed / 3 * Time.deltaTime);
+                // Come to a stop if close enough to the objective.
+                state = "IDLE";
             }
         } else if(state == "AGGRO") {
             waypoint = p.transform.position;
+            wanderTime = wanderTime + Time.deltaTime;
             currentSpeed = Mathf.Lerp(currentSpeed, movementSpeed, movementSpeed * Time.deltaTime);
+
+            // Take current angle and take angle toward player. Lerp the angle toward the player angle.
+            // If the angle is too much, go into "ORIENTATION"
+            // Vector2 direction = new Vector2(transform.position.x, transform.position.z) - new Vector2(p.transform.position.x, p.transform.position.z);
+            Quaternion intention = Quaternion.LookRotation(waypoint - transform.position, Vector3.up);
+            intention = Quaternion.Euler(new Vector3(0, intention.eulerAngles.y, 0));
+            // Quaternion.LookRotation(player.position - cam.transform.position, Vector3.up).eulerAngles;
+            Debug.Log(Quaternion.LookRotation(transform.forward, Vector3.up).eulerAngles.y - intention.eulerAngles.y);
+            if (Mathf.Abs(Quaternion.LookRotation(transform.forward, Vector3.up).eulerAngles.y - intention.eulerAngles.y) > 30 && wanderTime > 0.1f)
+            {
+                state = "ORIENTATION";
+                wanderTime = 0;
+            }
 
             // If close enough, queue up an attack???
         }
+        // Also important - leave room for an idle state to exist.
+        if (state == "IDLE")
+        {
+            // Debug.Log("!");
+            wanderTime = wanderTime + Time.deltaTime / 2;
+            currentSpeed = Mathf.Lerp(currentSpeed, 0, movementSpeed / 3 * Time.deltaTime);
+            if (wanderTime > maxWanderTime)
+            {
+                state = "WANDER";
+                NewWaypoint();
+                wanderTime = 0;
+            }
+        }
+    }
+
+    private void FixedUpdate() {
+        
+        // Handle drag - when idle, the brog should stop quickly.
+        if (state != "IDLE") {
+            transform.GetComponent<Rigidbody>().drag = 1;
+        } else {
+            transform.GetComponent<Rigidbody>().drag = 10;
+        }
+
+
+        // Turn if you're aggro and not spinning /OR/ if you're wandering
+        if ( (state == "AGGRO" && wanderTime > 0.1f) || state == "WANDER") {
+            Quaternion intention = Quaternion.LookRotation(waypoint - transform.position, Vector3.up);
+            intention = Quaternion.Euler(new Vector3(0, intention.eulerAngles.y, 0));
+            transform.GetComponent<Rigidbody>().MoveRotation(Quaternion.Slerp(Quaternion.LookRotation(transform.forward), intention, turnRate * Time.deltaTime));
+        }
 
         // Handle movement
-        if(state != "IDLE" && state != "ATTACK" && state != "ORIENTATION") {
-            transform.position = Vector3.MoveTowards(transform.position, waypoint, currentSpeed * Time.deltaTime);
+        if (state != "IDLE" && state != "ATTACK" && state != "ORIENTATION")
+        {
+            // transform.position = Vector3.MoveTowards(transform.position, waypoint, currentSpeed * Time.deltaTime);
+
+            transform.GetComponent<Rigidbody>().velocity = Vector3.Lerp(transform.GetComponent<Rigidbody>().velocity, transform.forward * currentSpeed, 3 * Time.deltaTime);
         }
 
         // Always tell it how fast to be goin
         anim.SetFloat("Speed", currentSpeed / movementSpeed);
         // Debug.Log(currentSpeed / movementSpeed);
 
-    }
+        // Fix rotation I guess
+        // transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y, 0);
 
-    private void FixedUpdate()
-    {
-        if(state == "AGGRO")
-        {
-            // transform.eulerAngles = new Vector3(transform.eulerAngles.x, Mathf.MoveTowardsAngle(transform.eulerAngles.y, ))
-        }
     }
 
     // Create a new spot for the Brog to go to.
@@ -117,7 +170,7 @@ public class Brogger : MonoBehaviour
             if(true) {
                 success = true;
                 waypoint = tempWaypoint;
-                Debug.Log(waypoint);
+                // Debug.Log(waypoint);
             }
         }
         return;
