8a064d01738ea19008a0dcc7736111a889a31402
Axel <stewaa5@rpi.edu>
2019-07-27 23:40:42 -0400

    Repurposed the Brogger script to be the Sneckenator behavior script. Snecken does most of the things it needs to, and in addition to that, it slithers!

--- Awful Summer Jam 19/Assets/Scripts/Sneckenator.cs
+++ Awful Summer Jam 19/Assets/Scripts/Sneckenator.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Brogger : MonoBehaviour
+public class Sneckenator : MonoBehaviour
 {
     public string state = "IDLE";
     private Vector3 home;
@@ -19,14 +19,16 @@ public class Brogger : MonoBehaviour
     public float spinTime;
 
     // Start is called before the first frame update
-    void Start() {
+    void Start()
+    {
         state = "WANDER";
 
         home = transform.position;
         waypoint = home;
 
         // Locate the player if p has no value (could easily by avoided with other stuff but for now)
-        if(p == null) {
+        if (p == null)
+        {
             p = GameObject.FindGameObjectWithTag("Player");
         }
     }
@@ -35,31 +37,16 @@ public class Brogger : MonoBehaviour
     void Update() {
         // Debug.Log(state);
 
-        // If near/can see a player, switch into "ORIENTATION", which switches into "AGGRO"
+        // If near/can see a player, switch into "AGGRO".
         float distance = Vector3.Distance(p.transform.position, transform.position);
-        if (distance < aggroDist && state != "AGGRO" && state != "ORIENTATION" && state != "ATTACK") {
-            state = "ORIENTATION";
+        if (distance < aggroDist && state != "AGGRO" && state != "ATTACK") {
+            state = "AGGRO";
             wanderTime = 0;
-        } else if (distance > aggroDist * 1.3f && state == "AGGRO") { // This factor will likely need to change.
+        }
+        else if (distance > aggroDist * 1.3f && state == "AGGRO") { // This factor will likely need to change.
             state = "WANDER";
             // It will slow down, then come to a stop at the player's position at the moment of loss of interest (assuming it can get there in time)
             wanderTime = 0;
-        }
-
-        // Get ready for some shit
-        if (state == "ORIENTATION") {
-            waypoint = p.transform.position;
-            wanderTime = wanderTime + Time.deltaTime;
-            if (wanderTime < spinTime) {
-                transform.GetComponent<Rigidbody>().MoveRotation(Quaternion.Euler(new Vector3(0, transform.eulerAngles.y + wanderTime * wanderTime * 500, 0)));
-            }
-            else {
-                Debug.Log(".");
-                // Okay, so get the look rotation toward the player, but then only use the Y component.
-                transform.GetComponent<Rigidbody>().MoveRotation(Quaternion.Euler(new Vector3(0, Quaternion.LookRotation(waypoint - transform.position, Vector3.up).eulerAngles.y, 0)));
-                state = "AGGRO";
-                wanderTime = 0;
-            }
         }
 
         // If wandering, make sure there is one waypoint.
@@ -67,7 +54,7 @@ public class Brogger : MonoBehaviour
             // Debug.Log(wanderTime);
             // If a waypoint is inaccessible, allow it to time out.
             wanderTime = wanderTime + Time.deltaTime;
-            if(wanderTime > maxWanderTime) {
+            if (wanderTime > maxWanderTime) {
                 NewWaypoint();
                 wanderTime = 0;
             }
@@ -76,32 +63,24 @@ public class Brogger : MonoBehaviour
             // If too close, make a new one with random changes to x and z.
 
             // When moving, we don't want to be as urgent as if chasing the player.
-            if(Vector2.Distance(new Vector2(transform.position.x, transform.position.z), new Vector2(waypoint.x, waypoint.z)) > 0.2f) {
+            if (Vector2.Distance(new Vector2(transform.position.x, transform.position.z), new Vector2(waypoint.x, waypoint.z)) > 0.2f) {
                 currentSpeed = Mathf.Lerp(currentSpeed, movementSpeed / 3, movementSpeed / 3 * Time.deltaTime);
             } else {
                 // Come to a stop if close enough to the objective.
                 state = "IDLE";
             }
-        } else if(state == "AGGRO") {
+        } else if (state == "AGGRO") {
+
+            // Circling player code not very good.
             waypoint = p.transform.position;
+            // waypoint.x = waypoint.x + 5/(1+wanderTime/3) * Mathf.Sin(wanderTime * 0.5f);
+            // waypoint.z = waypoint.z + 5/(1+wanderTime/3) * Mathf.Cos(wanderTime * 0.5f);
             wanderTime = wanderTime + Time.deltaTime;
             currentSpeed = Mathf.Lerp(currentSpeed, movementSpeed, movementSpeed * Time.deltaTime);
 
-            // Take current angle and take angle toward player. Lerp the angle toward the player angle.
-            // If the angle is too much, go into "ORIENTATION"
-            // Vector2 direction = new Vector2(transform.position.x, transform.position.z) - new Vector2(p.transform.position.x, p.transform.position.z);
-            Quaternion intention = Quaternion.LookRotation(waypoint - transform.position, Vector3.up);
-            intention = Quaternion.Euler(new Vector3(0, intention.eulerAngles.y, 0));
-            // Quaternion.LookRotation(player.position - cam.transform.position, Vector3.up).eulerAngles;
-            Debug.Log(Quaternion.LookRotation(transform.forward, Vector3.up).eulerAngles.y - intention.eulerAngles.y);
-            if (Mathf.Abs(Quaternion.LookRotation(transform.forward, Vector3.up).eulerAngles.y - intention.eulerAngles.y) > 30 && wanderTime > 0.1f) {
-                state = "ORIENTATION";
-                wanderTime = 0;
-            }
-
             // ==============================================================================================================================
             // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
-            
+
             // If close enough, queue up an attack???
             if (Vector2.Distance(new Vector2(transform.position.x, transform.position.z), new Vector2(waypoint.x, waypoint.z)) < 1) {
                 state = "ATTACK";
@@ -127,7 +106,7 @@ public class Brogger : MonoBehaviour
         if (state == "ATTACK") {
             // Attack time should be constant probably.
             wanderTime = wanderTime + Time.deltaTime;
-            if(wanderTime > 1) {
+            if (wanderTime > 1) {
                 state = "AGGRO";
                 wanderTime = 0;
             }
@@ -135,32 +114,25 @@ public class Brogger : MonoBehaviour
     }
 
     private void FixedUpdate() {
-        
-        // Handle drag - when idle, the brog should stop quickly.
-        if (state != "IDLE") {
-            transform.GetComponent<Rigidbody>().drag = 1;
-        } else {
-            transform.GetComponent<Rigidbody>().drag = 10;
-        }
 
+        // Snecken's drag coefficient shouldn't change.
 
-        // Turn if you're aggro and not spinning /OR/ if you're wandering
-        if ( (state == "AGGRO" && wanderTime > 0.1f) || state == "WANDER") {
+        // Turn if you're aggro /OR/ if you're wandering
+        if ((state == "AGGRO" && wanderTime > 0.1f) || state == "WANDER") {
             Quaternion intention = Quaternion.LookRotation(waypoint - transform.position, Vector3.up);
-            intention = Quaternion.Euler(new Vector3(0, intention.eulerAngles.y, 0));
+            intention = Quaternion.Euler(new Vector3(0, intention.eulerAngles.y + 20 * Mathf.Sin(wanderTime*3), 0));
             transform.GetComponent<Rigidbody>().MoveRotation(Quaternion.Slerp(Quaternion.LookRotation(transform.forward), intention, turnRate * Time.deltaTime));
         }
 
         // Handle movement
-        if (state != "IDLE" && state != "ATTACK" && state != "ORIENTATION")
-        {
+        if (state != "IDLE" && state != "ATTACK") {
             // transform.position = Vector3.MoveTowards(transform.position, waypoint, currentSpeed * Time.deltaTime);
 
             transform.GetComponent<Rigidbody>().velocity = Vector3.Lerp(transform.GetComponent<Rigidbody>().velocity, transform.forward * currentSpeed, 3 * Time.deltaTime);
         }
 
         // Always tell it how fast to be goin
-        anim.SetFloat("Speed", currentSpeed / movementSpeed);
+        // anim.SetFloat("Speed", currentSpeed / movementSpeed);
         // Debug.Log(currentSpeed / movementSpeed);
 
         // Fix rotation I guess
@@ -168,12 +140,12 @@ public class Brogger : MonoBehaviour
 
     }
 
-    // Create a new spot for the Brog to go to.
+    // Create a new spot for the Snecken to go to.
     void NewWaypoint() {
         Vector3 tempWaypoint;
         bool success = false;
         // For now, assuming all points are good. Maybe could use the good navmesh to ratify generated points, or bad navmesh to eliminate.
-        while(!success) {
+        while (!success) {
 
             // Uniform distribution of waypoints within a radius.
             float angle = Random.value * 2 * Mathf.PI;
@@ -181,7 +153,7 @@ public class Brogger : MonoBehaviour
             tempWaypoint = new Vector3(home.x + radius * Mathf.Cos(angle), home.y, home.z + radius * Mathf.Sin(angle)); // This should work!???!??!
 
             // Ratify position placeholder
-            if(true) {
+            if (true) {
                 success = true;
                 waypoint = tempWaypoint;
                 // Debug.Log(waypoint);
