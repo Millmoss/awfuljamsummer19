bf8d29a43aea546178d4a01946df1876fe5881dd
Millmoss <omelliff@yahoo.com>
2019-07-29 22:17:05 -0400

    AAAAAA wer' e done

--- Awful Summer Jam 19/Assets/Scripts/ONO.cs
+++ Awful Summer Jam 19/Assets/Scripts/ONO.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ONO : MonoBehaviour
 {
 	int h = 10;
 	public Animator a;
+	public float t = 0;
+	public GameObject pp;
+	public AudioSource aa;
+	public AudioSource b;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +25,22 @@ public class ONO : MonoBehaviour
 		{
 			a.SetFloat("Blend", 1);
 			gameObject.AddComponent<Rigidbody>().AddForce(new Vector3(Random.value * 10, Random.value * 10, Random.value * 10));
+			b.Play();
+		}
+
+		if (t != 0)
+			t += Time.deltaTime;
+
+		if (t > 60f)
+		{
+			SceneManager.LoadScene("THATSIT");
+		}
+
+		if ((pp.transform.position - transform.position).magnitude < 20)
+		{
+			if (t == 0)
+				aa.Play();
+			t += Time.deltaTime;
 		}
     }
 
